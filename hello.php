b54d13777bff4abbfa800d8cdec5b85354f409cb
bothmagic@gmail.com <bothmagic@gmail.com@023842cf-2098-fd4c-1e87-83fa5642b562>
2012-11-12 15:59:05 +0000

    .

--- hello.php
+++ hello.php
@@ -1,24 +1,8 @@
-﻿ <?php
+  <?php
 
     require("Template.class.php");
 
     $tpl = new Template("hello.html");
-    
-    // Vamos supor que esta quantidade veio do banco de dados
-    $quantidade = 5;
-    
-    // Se existem produtos cadastrados, vamos exibir a quantidade
-    if($quantidade > 0){
-        $tpl->QUANTIDADE = $quantidade;
-        $tpl->block("BLOCK_QUANTIDADE");
-
-    }
-    
-    // Caso não exista nenhum produto, exibimos a mensagem de vazio
-    else {
-        $tpl->block("BLOCK_VAZIO");
-    }
-    
     
     $tpl->show();
     
